d6e90bb7b7991e54fd696269a935166191425663
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-20 18:31:52 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
-# code here!
+class School 
+  attr_reader :roster 
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end 
+  
+  def add_student(student, grade)
+    @roster[grade] ||=[]
+    @roster[grade] << student 
+  end 
+  
+  def grade(grade)
+    
+    
+  end 
+  
+end
